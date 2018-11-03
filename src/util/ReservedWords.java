7bfc16c199870df825aaddb0e3e53832c125bc1e
161250181 <498690270@qq.com>
2018-11-04 01:08:44 +0800

    Initial commit

---
+++ src/util/ReservedWords.java
@@ -0,0 +1,8 @@
+package util;
+
+public class ReservedWords {
+    public static String[] reservedWords = {"class", "public", "protected", "private",
+            "static", "final", "void", "new", "int", "char", "float",
+            "double", "String", "if", "else", "switch", "case",
+            "break", "for", "do", "while", "continue", "try", "catch"};
+}
---
+++ src/util/Token.java
@@ -0,0 +1,25 @@
+package util;
+
+public class Token {
+    private int code;
+    private String string;
+    private String error;
+
+    public Token(int code, String string){
+        this.code = code;
+        this.string = string;
+        this.error = null;
+    }
+    public Token(String error){
+        this.error = error;
+    }
+
+    public String toString() {
+        if(this.error != null){     //是错误
+            return this.error;
+        }
+        else{
+            return "<"+ this.string + ", " + this.code + ">";
+        }
+    }
+}
